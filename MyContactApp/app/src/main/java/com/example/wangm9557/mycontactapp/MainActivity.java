0d5a0bed44bc42022bedda00e6b51c2b7cfe9192
mzwang255 <WangM9557@sduhsd.lan>
2017-05-12 09:23:00 -0700

    Updated database helper and MainActivity to create a Database.
    Michael Wang - Added main field and add button

--- MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.wangm9557.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataBaseHelper myDB;
+    EditText editName;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDB = new DataBaseHelper(this);
     }
 }
