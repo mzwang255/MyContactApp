bb2e1f5e2741ddd1d2674c4d1da61aa3ee763189
mzwang255 <WangM9557@sduhsd.lan>
2017-05-16 09:21:43 -0700

    Added Contacts Button; New Method viewData and showMessage and getAllData
    Added Token Messages and button to see what information is added

--- MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/DataBaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/DataBaseHelper.java
@@ -2,6 +2,7 @@ package com.example.wangm9557.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -44,5 +45,11 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         long result = db.insert(TABLE_NAME, null, contentValues);
         if(result == -1) return false;
         else return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
--- MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wangm9557.mycontactapp;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     EditText editAddress;
     EditText editAge;
     Button btnAddData;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void addData(View v){
         boolean isInserted = myDb.insertData(editName.getText().toString(), editAddress.getText().toString(), editAge.getText().toString() );
+
         int duration = Toast.LENGTH_SHORT;
         Toast toastSuccessful = Toast.makeText(this, "Inserted Correctly", duration);
         Toast toastNotSuccessful = Toast.makeText(this, "Inserted Incorrectly", duration);
+
         if(isInserted){
             Log.d("MyContact", "Data Insertion Successful");
             // create toast message to user indicating data inserted correctly
@@ -40,10 +42,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else {
-            Log.d("MyContact", "Data Insertion inuccessful");
+            Log.d("MyContact", "Data Insertion insuccessful");
             // create toast message to user indicating data inserted incorrectly
             toastNotSuccessful.show();
         }
+    }
+
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found in database");
+            //put a log.d message and toast
+            Log.d("MyContact", "No Data found in database");
+
+            int duration = Toast.LENGTH_SHORT;
+            Toast noData = Toast.makeText(this, "No Data Found in Database", duration);
+            noData.show();
+
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //Setup loop with Cursor moveToNext method
+        //  append each COL to the buffer
+        //  use getStringMethod
+
+        while(res.moveToNext()){
+            buffer.append("Name: " + res.getString(1));
+            buffer.append(" /// Address: " + res.getString(2));
+            buffer.append(" /// Age: " + res.getString(3));
+            buffer.append("\n\n");
+        }
+
+
+
+        showMessage("Data", buffer.toString());
+        Log.d("MyContact", buffer.toString());
+    }
+
+    private void showMessage(String title, String message) {
+
     }
 
 }
