2dc68927b0b8f64620da1bbcca84844000922945
mzwang255 <WangM9557@sduhsd.lan>
2017-05-18 09:19:24 -0700

    Added an Area for User to search text, and button to search.
    Added a new method searchContact to run another new method
    returnSearchContact which returns the search results. Basically I
    programmed the app to do something when user searches for something with
    the text area and search button.

--- MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/wangm9557/mycontactapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wangm9557.mycontactapp;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +13,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     DataBaseHelper myDb;
     EditText editName;
     EditText editAddress;
     EditText editAge;
     Button btnAddData;
+    EditText searchName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         editName = (EditText) findViewById(R.id.editText_name);
         editAddress = (EditText) findViewById(R.id.editText_address);
         editAge = (EditText) findViewById(R.id.editText_age);
+        searchName = (EditText) findViewById(R.id.editText_searchText);
     }
 
     public void addData(View v){
@@ -70,9 +74,9 @@ public class MainActivity extends AppCompatActivity {
 
         while(res.moveToNext()){
             buffer.append("ID: " + res.getString(0));
-            buffer.append(" /// Name: " + res.getString(1));
-            buffer.append(" /// Address: " + res.getString(2));
-            buffer.append(" /// Age: " + res.getString(3));
+            buffer.append(" || Name: " + res.getString(1));
+            buffer.append(" || Address: " + res.getString(2));
+            buffer.append(" || Age: " + res.getString(3));
             buffer.append("\n\n");
         }
 
@@ -80,6 +84,42 @@ public class MainActivity extends AppCompatActivity {
 
         showMessage("Data", buffer.toString());
         Log.d("MyContact", buffer.toString());
+    }
+
+    private String returnSearchContact(){
+        String search = searchName.getText().toString();
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found in database");
+            //put a log.d message and toast
+            Log.d("MyContact", "No Data found in database");
+
+            int duration = Toast.LENGTH_SHORT;
+            Toast noData = Toast.makeText(this, "No Data Found in Database", duration);
+            noData.show();
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //Setup loop with Cursor moveToNext method
+        //  append each COL to the buffer
+        //  use getStringMethod
+
+       while(res.moveToNext()){
+
+            if(res.getString(1).toUpperCase().equals(search.toUpperCase())){
+                return ("Name: " + res.getString(1)
+                        + " || Address: " + res.getString(2)
+                        + " || Age: " + res.getString(3));
+            }
+        }
+
+
+        return "Not Found";
+    }
+
+    public void searchContact(View v){
+        showMessage("Result", returnSearchContact());
+        Log.d("Result", returnSearchContact());
     }
 
     private void showMessage(String title, String message) {
